acde6856a8b1c3ae1289bfde3f8d40ceefeba1d9
Lazur Niko <lazur.niko@gmail.com>
2023-02-23 15:52:32 +0200

    LiveChat bot

--- public/js/main.js
+++ public/js/main.js
@@ -1,11 +1,17 @@
 const chatForm = document.getElementById('chat-form');
 const chatMessages = document.querySelector('.chat-messages');
 
+const { username, room } = Qs.parse(location.search, {
+  ignoreQueryPrefix: true
+});
+
 const socket = io();
+
+socket.emit('joinRoom', { username, room })
 
 socket.on('message', message => {
   outputMessage(message);
-
+  console.log(message)
   chatMessages.scrollTop = chatMessages.scrollHeight;
 })
 
@@ -23,8 +29,9 @@ chatForm.addEventListener('submit', e => {
 function outputMessage(message) {
   const div = document.createElement('div');
   div.classList.add('message');
-  div.innerHTML = `<p class="text">
-			${message}
-		</p>`;
+  div.innerHTML = `<p class="meta">${message.username}<span class="meta-time"> ${message.time}</span></p>
+    <p class="text">
+      ${message.text}
+    </p>`;
   document.querySelector('.chat-messages').appendChild(div)
 }
--- server.js
+++ server.js
@@ -2,25 +2,37 @@ const path = require("path");
 const express = require('express');
 const http = require('http');
 const socketio = require("socket.io");
+const formatMessage = require('./utils/messages.js');
+const { userJoin, getCurrentUser }= require('./utils/users.js')
 
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
 
 app.use(express.static(path.join(__dirname, 'public')));
+const botName = 'LiveChat bot';
 
 io.on('connection', socket => {
-  socket.emit('message', 'Welcome to Live Chat');
 
-  socket.broadcast.emit('message', "A user has join the chat");
+  socket.on('joinRoom',({username, room}) => {
+    const user = userJoin(socket.id, username, room)
+    socket.join(user.room);
+    
+    socket.emit('message', formatMessage(botName, 'Welcome to Live Chat'));
 
-  socket.on('disconnect', () => {
-    io.emit("message", "User has left the chat");
-  });
+    socket.broadcast.to(user.room).emit('message', formatMessage(botName,"A user has join the chat"));
+  })
+
+  
+
+  
 
   socket.on('chatMessage', msg => {
-    io.emit('message', msg);
+    io.emit('message', formatMessage('USER', msg));
   })
+  socket.on('disconnect', () => {
+    io.emit("message", formatMessage(botName,"User has left the chat"));
+  });
 });
 
 const PORT = 3000 || process.env.PORT;
